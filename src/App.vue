cf57d1f3bb3d41bc2b0ac37c08d72496190e402f
Ansaal <jhulsch.jh@googlemail.com>
2022-06-13 13:07:25 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-app>
+    <v-navigation-drawer app>
+      <!-- -->
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <!-- -->
+    </v-app-bar>
+
+    <!-- Sizes your content based upon application components -->
+    <v-main>
+
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+
+        <!-- If using vue-router -->
+       <v-btn @click="counter++">{{counter}}</v-btn>
+      </v-container>
+    </v-main>
+
+    <v-footer app>
+      <!-- -->
+    </v-footer>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+
+import {ref} from "@vue/composition-api";
+
+const counter = ref(0);
+
+
+</script>
+
+
+
+<style>
+body {
+  font-family: Roboto, sans-serif;
+}
+
+</style>
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import Vue from 'vue';
+import App from './App.vue';
+import VueCompositionAPI from '@vue/composition-api'
+import vuetify from "./plugins/vuetify";
+Vue.use(VueCompositionAPI)
+
+new Vue({
+    vuetify,
+    render: (h) => h(App),
+}).$mount('#app');
